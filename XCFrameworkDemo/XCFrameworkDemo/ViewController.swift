b29ce41ad58028738422a7f7d7aee28c722471b8
Anurag Ajwani <anurag.ajwani@gmail.com>
2020-06-01 02:02:40 +0100

    Added XCFrameworkDemo app

---
+++ XCFrameworkDemo/XCFrameworkDemo/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  XCFrameworkDemo
+//
+//  Created by Anurag Ajwani on 31/05/2020.
+//  Copyright © 2020 Anurag Ajwani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        // Present LoginViewController here
+    }
+}
